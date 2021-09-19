66374b23c2db7b2142c78cb81b2eefff0ad22af8
Alberto <albertus82@users.noreply.github.com>
2021-09-19 22:03:43 +0200

    Added version date in -V output.

--- src/main/java/com/github/albertus82/wget/VersionProvider.java
+++ src/main/java/com/github/albertus82/wget/VersionProvider.java
@@ -1,12 +1,30 @@
 package com.github.albertus82.wget;
 
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.TemporalAccessor;
+import java.util.logging.Level;
+
+import lombok.extern.java.Log;
 import picocli.CommandLine.IVersionProvider;
 
+@Log
 public class VersionProvider implements IVersionProvider {
 
 	@Override
 	public String[] getVersion() {
-		return new String[] { "${COMMAND-FULL-NAME} " + BuildInfo.getProperty("project.version") };
+		return new String[] { "${COMMAND-FULL-NAME} " + BuildInfo.getProperty("project.version") + " (" + DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).format(getVersionTimestamp()) + ')' };
+	}
+
+	private static TemporalAccessor getVersionTimestamp() {
+		try {
+			return DateTimeFormatter.ISO_ZONED_DATE_TIME.parse(BuildInfo.getProperty("version.timestamp"));
+		}
+		catch (final RuntimeException e) {
+			log.log(Level.FINE, "Invalid version timestamp, falling back to current datetime:", e);
+			return Instant.now();
+		}
 	}
 
 }
