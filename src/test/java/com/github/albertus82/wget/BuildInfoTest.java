bf8304bdd20ef558b98b7aec0e0b60a1274d5a6e
Alberto <albertus82@users.noreply.github.com>
2021-09-20 14:28:13 +0200

    Test.

--- src/test/java/com/github/albertus82/wget/BuildInfoTest.java
+++ src/test/java/com/github/albertus82/wget/BuildInfoTest.java
@@ -1,5 +1,8 @@
 package com.github.albertus82.wget;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.TreeSet;
 import java.util.UUID;
 
 import org.junit.jupiter.api.Assertions;
@@ -7,17 +10,19 @@ import org.junit.jupiter.api.Test;
 
 class BuildInfoTest {
 
-	private static final String[] expectedPropertyNames = new String[] { "project.artifactId", "project.version", "version.timestamp" };
+	private static final Collection<String> expectedPropertyNames = Arrays.asList("project.artifactId", "project.version", "version.timestamp");
 
 	@Test
-	void testBuildInfo() {
-		Assertions.assertNotEquals(0, BuildInfo.INSTANCE.properties.size());
+	void testApi() {
 		Assertions.assertNull(BuildInfo.getProperty(UUID.randomUUID().toString()));
 		Assertions.assertThrows(NullPointerException.class, () -> BuildInfo.getProperty(null));
+		Assertions.assertDoesNotThrow(() -> BuildInfo.getProperty(""));
 	}
 
 	@Test
 	void testExpectedProperties() {
+		Assertions.assertEquals(expectedPropertyNames.size(), BuildInfo.INSTANCE.properties.size());
+		Assertions.assertEquals(new TreeSet<>(expectedPropertyNames), new TreeSet<>(BuildInfo.INSTANCE.properties.keySet()));
 		for (final String name : expectedPropertyNames) {
 			final String value = BuildInfo.getProperty(name);
 			Assertions.assertNotNull(value);
